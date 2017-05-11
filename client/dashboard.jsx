78e2b65fa1b305f355805084e2ae83ed04c1e71f
Marcus Chevalier <CueBall@Marcuss-MacBook-Pro.local>
2017-05-10 21:12:24 -0500

    add audio recording

--- client/dashboard.jsx
+++ client/dashboard.jsx
@@ -1,7 +1,38 @@
 import React, { Component } from 'react';
+import Recorder from 'react-recorder'
 
 export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      command: 'start',
+      audioFile: false,
+    };
+    this.onStop = this.onStop.bind(this);
+  }
+
+  onStop(blob) {
+    var reader = new window.FileReader();
+    reader.readAsDataURL(blob);
+    reader.onloadend = function() {
+      this.setState({ audioFile: reader.result });
+    }.bind(this);
+  }
+
   render() {
-    return <div>Dashboard</div>;
+    return (
+      <div>
+      <Recorder onStop={this.onStop} command={this.state.command}/>
+      <button onClick={() => this.setState({ command: 'start' })}>Start</button>
+      <button onClick={() => this.setState({ command: 'stop' })} >Stop</button>
+      <button>Play</button>
+      {
+        this.state.audioFile ?
+          (
+            <audio controls="controls" src={this.state.audioFile} />
+          ) : ''
+      }
+      </div>
+    );
   }
 }
